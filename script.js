38e2138c3ac9c16521a55fb716aa699554ebc532
Timóteo Mendonça Neto <timoteo.m@uni9.edu.br>
2021-10-19 16:16:24 -0300

    validação

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ function converter(){
     var bin = document.getElementById("bin").value;
     var decimal = 0;
 
-    for(let i = bin.length-1; i >= 0; i--){
-        decimal += parseInt(bin.charAt(i)) * Math.pow(2, bin.length-1-i);
-    }
+
+    if (bin === '') return alert('Eh preciso digitar algo');
+    bin.split('').map((char) => {
+        if (char !== '0' && char !== '1') return alert('Por favor, digite um número binário');
+        for(let i = bin.length-1; i >= 0; i--){
+            decimal += parseInt(bin.charAt(i)) * Math.pow(2, bin.length-1-i);
+        }
+    
+        document.getElementById("resposta").innerHTML = decimal;
+    });
 
 
-    document.getElementById("resposta").innerHTML = decimal;
 }
